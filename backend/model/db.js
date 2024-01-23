6b64c404b84c95a6499a9d44b7829d4a0e07deaa
Shantanu Kaushik <shantanukaushik12@gmail.com>
2024-01-23 21:56:48 +0530

    update db.js

--- backend/model/db.js
+++ backend/model/db.js
@@ -1,13 +1,27 @@
-const { Pool } = require('pg');
+const { Sequelize } = require("sequelize");
 
-const pool = new Pool({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_NAME,
+//import models
+console.log(process.env.DB_NAME)
+const db = {};
+const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:5432/${process.env.DB_NAME}`, {
+	logging: () => process.env.NODE_ENV === 'development'? true : false
 });
 
-module.exports = {
-    query: (text, params) => pool.query(text, params)
-};
+console.log(sequelize);
+
+try {
+	//store sequelize stuff in the DB
+	db.Sequelize = Sequelize;
+	db.sequelize = sequelize;
+
+	sequelize.authenticate();
+	if(process.env.NODE_ENV === 'development') console.log("Connection has been established successfully.");
+
+	// db.users = require("./UserModel")(sequelize);
+
+} catch (error) {
+	console.error("Unable to connect to the database:", error);
+}
+
+//exporting the module
+module.exports = db;
